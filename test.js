94ac2f14c834c10b1e0d4fb62c49acdd69618d84
jordangarcia <jordan.garcia@gmail.com>
2014-12-12 13:55:25 -0800

    Add support for object-concise-method

--- test.js
+++ test.js
@@ -17,3 +17,19 @@ it('should convert ES6 to ES5', function (cb) {
                 contents: new Buffer("var empty = () => {};")
         }));
 });
+
+it('should convert object concise methods', function (cb) {
+
+        var stream = jstransform("var obj = { method() {} };");
+
+        stream.on('data', function (file) {
+                assert.equal(file.relative, 'fixture.js');
+                assert.equal(file.contents.toString(), 'var obj = { method:function() {} };');
+                cb();
+        });
+
+        stream.write(new gutil.File({
+                path: 'fixture.js',
+                contents: new Buffer("var obj = { method() {} };")
+        }));
+});
